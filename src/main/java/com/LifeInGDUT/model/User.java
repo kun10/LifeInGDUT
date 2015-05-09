139be67add8bc5e2e46d1e64e713956215f09325
robbin <779822254@qq.com>
2015-05-09 17:46:41 +0800

    User

--- src/main/java/com/LifeInGDUT/model/User.java
+++ src/main/java/com/LifeInGDUT/model/User.java
@@ -1,10 +1,51 @@
 package com.LifeInGDUT.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User {
 
+	private int id;
+	private String name;
+	private char[] password;
+	private String email;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
