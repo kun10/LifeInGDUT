9af8c9ac1cdfe978393ad6a9d3e4e55155307941
kun10 <441225695@qq.com>
2015-05-09 17:59:37 +0800

    做好大部分repair

--- src/main/java/com/LifeInGDUT/controller/RepairController.java
+++ src/main/java/com/LifeInGDUT/controller/RepairController.java
@@ -1,9 +1,14 @@
 package com.LifeInGDUT.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.LifeInGDUT.model.Repair;
 import com.LifeInGDUT.service.RepairService;
@@ -18,6 +23,14 @@ public class RepairController {
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	public void addRepair(Repair repair){
 		rService.add(repair);
+	}
+	
+	@RequestMapping(value="/showRepair", method=RequestMethod.POST)
+	public String showRepair(@RequestParam int pageNumber, @RequestParam int page_size, @RequestParam String status, HttpServletResponse response){
+		List<Repair> repairs = rService.getRepairByStatus(pageNumber, page_size, status);
+		net.sf.json.JSONArray json = net.sf.json.JSONArray.fromObject(repairs);
+		System.out.println(json.toString());
+		return json.toString();
 	}
 	
 }
--- src/main/java/com/LifeInGDUT/service/RepairService.java
+++ src/main/java/com/LifeInGDUT/service/RepairService.java
@@ -1,5 +1,7 @@
 package com.LifeInGDUT.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,18 @@ public class RepairService {
 	public void add(Repair repair){
 		repair.setStatus("处理中");
 		repairDao.insert(repair);
+	}
+	
+	/**
+	 * 根据status分页查询数据
+	 * @param pageNumber	页码
+	 * @param page_size	每一页显示数量
+	 * @param status
+	 * @return
+	 */
+	public List<Repair> getRepairByStatus(int pageNumber, int page_size, String status){
+		int first = (pageNumber-1)*page_size;
+		return repairDao.selectByStatus(first, page_size, status);
 	}
 	
 }
