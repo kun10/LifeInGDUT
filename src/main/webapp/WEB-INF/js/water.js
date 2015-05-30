98b9a4504ef06bfbaff60f5758c9e2aa223e0b39
robbin <779822254@qq.com>
2015-05-31 00:11:25 +0800

    conflict

--- src/main/webapp/WEB-INF/js/water.js
+++ src/main/webapp/WEB-INF/js/water.js
@@ -1,160 +1,212 @@
-$(document).ready(function(){
-	var main=document.getElementById('show-main');
-	main.onclick=function(e){
-		e=e||window.event;     
-		var el=e.srcElement;
-		var el=e.target;		//兼容火狐
-		switch(el.className){
-			//点击分页页数按钮
-			case 'pagehref':judgePage(el);break;
+$(document).ready(function() {
+	var main = document.getElementById('show-main');
+	main.onclick = function(e) {
+		e = e || window.event;
+		var el = e.srcElement;
+		var el = e.target; // 兼容火狐
+		switch (el.className) {
+		// 点击分页页数按钮
+		case 'pagehref':
+			judgePage(el);
+			break;
 		}
 	}
-	//点击复选框全选事件
-	$("#checkallbox").click(function(){
+	// 点击复选框全选事件
+	$("#checkallbox").click(function() {
 		changeBoxCheck();
 	})
-	$(".checkbox").click(function(){
+	$(".checkbox").click(function() {
 		changeboxCheck();
 	})
-	//确认接单按钮
-	$(".acceptOrder").click(function(){
+	// 确认接单按钮
+	$(".acceptOrder").click(function() {
 		checkSelectBox();
 	})
-	//确认送达按钮
-	$(".checkBoxChoose").click(function(){
+	// 确认送达按钮
+	$(".checkBoxChoose").click(function() {
 		checkBox();
 	})
-	//点击确认充值按钮
-	$(".water-charge-button").click(function(){
-		var stuid=$("#studentId").val();
-		var waternumber=$("#waterNumbers").val();
-		if(stuid=="")	$("#stuIdInform").text("请输入学号");
-		else if(stuid!=""&&isNaN(stuid))		$("#stuIdInform").text("请输入正确学号");
-		else if(stuid!="")	$("#stuIdInform").text("");
-		if(waternumber=="")	$("#numberInform").text("请输入充值桶数");
-		else if(waternumber!=""&&isNaN(waternumber))	$("#numberInform").text("请输入正确数字");
-		else if(waternumber!="")	$("#numberInform").text("");
-		if(stuid!=""&&waternumber!=""&&!isNaN(waternumber)){
-			$("#mask").css('height',$(document).height()).show();
+	// 点击确认充值按钮
+	$(".water-charge-button").click(function() {
+		var stuid = $("#studentId").val();
+		var waternumber = $("#waterNumbers").val();
+		if (stuid == "")
+			$("#stuIdInform").text("请输入学号");
+		else if (stuid != "" && isNaN(stuid))
+			$("#stuIdInform").text("请输入正确学号");
+		else if (stuid != "")
+			$("#stuIdInform").text("");
+		if (waternumber == "")
+			$("#numberInform").text("请输入充值桶数");
+		else if (waternumber != "" && isNaN(waternumber))
+			$("#numberInform").text("请输入正确数字");
+		else if (waternumber != "")
+			$("#numberInform").text("");
+		if (stuid != "" && waternumber != "" && !isNaN(waternumber)) {
+			$("#mask").css('height', $(document).height()).show();
 			$(".sure-stuid").text(stuid);
 			$(".sure-number").text(waternumber);
-			$(".sure-money").text(waternumber*7+' 元');
+			$(".sure-money").text(waternumber * 7 + ' 元');
 			$(".water-charge-surebox").show();
 		}
 	})
-	//点击充值信息确认框关闭按钮
-	$(".water-charge-sureclose").click(function(){
+	// 点击充值信息确认框关闭按钮
+	$(".water-charge-sureclose").click(function() {
 		$("#stuIdInform").text("");
 		$("#numberInform").text("");
 		$(".water-charge-surebox").hide();
 		$("#mask").hide();
 	})
-	//点击充值信息确认框确认按钮
-	$(".water-charge-infor-button").click(function(){
-		$("#studentId").val('');
-		$("#waterNumbers").val('');
-		$("#stuIdInform").text("");
-		$("#numberInform").text("");
-		$(".water-charge-surebox").hide();
-		$("#mask").hide();
+	// 点击充值信息确认框确认按钮
+	$(".water-charge-infor-button").click(function() {
+		var id = $("#studentId").val();
+		var num = $("#waterNumbers").val();
+		$.post("/LifeInGDUT/water/charge", {
+			studentId : id,
+			number : num
+		}, function(data, status) {
+			if (data == "success") {
+				alert('充值成功');
+				$("#studentId").val('');
+				$("#waterNumbers").val('');
+				$("#stuIdInform").text("");
+				$("#numberInform").text("");
+				$(".water-charge-surebox").hide();
+				$("#mask").hide();
+			} else {
+				alert('充值失败，用户不存在')
+			}
+		});
 	});
 })
-//body加载相关函数
-	var totalpage,pagesize,cpage,count,curcount,outstr;
-	var checkAllBox=document.getElementById('checkallbox');
-	var checkboxes=document.getElementsByName('checkbox');
-	var allOrder=8;
-	pagesize = 10; 		//总共可以显示多少页
-	outstr = ""; 		//字符串，用来存放生成的页码
-	//设置页数函数
-	function pagesNumberSet(nowpage,allpage){
-		cpage=nowpage;
-		totalpage=allpage;
-		setpage();
+// body加载相关函数
+var totalpage, pagesize, cpage, count, state, curcount, outstr, url;
+var checkAllBox = document.getElementById('checkallbox');
+var checkboxes = document.getElementsByName('checkbox');
+var allOrder = checkboxes.length;
+pagesize = 10; // 总共可以显示多少页
+outstr = ""; // 字符串，用来存放生成的页码
+// 设置页数函数
+function pagesNumberSet(nowpage, allpage, nowstate, path) {
+	cpage = nowpage;
+	totalpage = allpage;
+	state = nowstate;
+	url = path;
+	setpage();
+}
+// 创建页数函数
+function judgePage(el) {
+	var $el = $(el);
+	var elText = $el.text();
+	if (!isNaN(elText))
+		gotopage(elText);
+	else if (elText == "下一页") {
+		if (cpage != totalpage)
+			gotopage(parseInt(cpage) + 1);
+	} else if (elText == "上一页") {
+		if (cpage != 1)
+			gotopage(parseInt(cpage) - 1);
 	}
-    //创建页数函数
-	function judgePage(el){
-		var $el=$(el);
-		var elText=$el.text();
-		if(!isNaN(elText))	gotopage(elText);
-		else if(elText=="下一页"){
-			if(cpage!=totalpage)	gotopage(parseInt(cpage)+1);
-		}
-		else if(elText=="上一页"){
-			if(cpage!=1)	gotopage(parseInt(cpage)-1);
-		}
+}
+function gotopage(pages) {
+	cpage = pages; // 把页面计数定位到第几页
+	setpage();
+}
+function setpage() {
+	if(url == 0) {
+		url = "<a href='/LifeInGDUT/water/getOrder?state=" + state
+		+ "&page=" + (cpage - 1) + "' class='pagehref'>上一页</a>";
 	}
-	function gotopage(pages){   
-	    cpage = pages;        //把页面计数定位到第几页
-	    setpage(); 
-	} 
-	function setpage(){ 
-		outstr = outstr + "<a href=#' class='pagehref'>上一页</a>"; 
-	    if(totalpage<=10){        //总页数小于十页 
-	        for (count=1;count<=totalpage;count++) setOutstr();
-	    } 
-	    if(totalpage>10){        //总页数大于十页 
-	        if(parseInt((cpage-1)/10) == 0){            
-	            for (count=1;count<=10;count++) setOutstr(); 
-	        } 
-	        else if(parseInt((cpage-1)/10) == parseInt(totalpage/10)) {     
-	            for (count=parseInt(totalpage/10)*10+1;count<=totalpage;count++) setOutstr();      
-	        } 
-	        else{     
-	            for (count=parseInt((cpage-1)/10)*10+1;count<=parseInt((cpage-1)/10)*10+10;count++)		setOutstr();
-	        } 
-	    }     
-	    outstr = outstr + "<a href='#' class='pagehref'>下一页</a>"; 
-	    document.getElementById("setpage").innerHTML = "<div id='setpage'><span id='info'>共"+totalpage+"页|第"+cpage+"页<\/span>" + outstr + "<\/div>"; 
-	    outstr = ""; 
+	if (cpage > 1)
+		outstr = outstr + url;
+	if (totalpage <= 10) { // 总页数小于十页
+		for (count = 1; count <= totalpage; count++)
+			setOutstr();
 	}
-	//根据页码对字符串outstr进行拼接处理
-	function setOutstr(){
-		if(count!=cpage) 
-			outstr = outstr + "<a href='#' class='pagehref'>"+count+"</a>"; 
-	    else
-	        outstr = outstr + "<span class='current' >"+count+"</span>";  
-	}
-	//将复选框改为全选或全不选函数
-	function changeBoxCheck(){
-		if(checkAllBox.checked){
-			for(var index=0;index<checkboxes.length;index++){
-				checkboxes[index].checked=true;
-			}
-		}
-		else if(!checkAllBox.checked){
-			for(var index=0;index<checkboxes.length;index++){
-				checkboxes[index].checked=false;
-			}
+	if (totalpage > 10) { // 总页数大于十页
+		if (parseInt((cpage - 1) / 10) == 0) {
+			for (count = 1; count <= 10; count++)
+				setOutstr();
+		} else if (parseInt((cpage - 1) / 10) == parseInt(totalpage / 10)) {
+			for (count = parseInt(totalpage / 10) * 10 + 1; count <= totalpage; count++)
+				setOutstr();
+		} else {
+			for (count = parseInt((cpage - 1) / 10) * 10 + 1; count <= parseInt((cpage - 1) / 10) * 10 + 10; count++)
+				setOutstr();
 		}
 	}
-	function changeboxCheck(){
-		var sum=0;
-		for(var index=0;index<checkboxes.length;index++){
-			if(checkboxes[index].checked==true){
-				sum++;
-			}
+	if (cpage < totalpage)
+		outstr = outstr + "<a href='/LifeInGDUT/water/getOrder?state=" + state
+				+ "&page=" + (cpage + 1) + "' class='pagehref'>下一页</a>";
+	document.getElementById("setpage").innerHTML = "<div id='setpage'><span id='info'>共"
+			+ totalpage + "页|第" + cpage + "页<\/span>" + outstr + "<\/div>";
+	outstr = "";
+}
+// 根据页码对字符串outstr进行拼接处理
+function setOutstr() {
+	if (count != cpage)
+		outstr = outstr + "<a href='/LifeInGDUT/water/getOrder?state=" + state
+				+ "&page=" + count + "' class='pagehref'>" + count + "</a>";
+	else
+		outstr = outstr + "<span class='current' >" + count + "</span>";
+}
+// 将复选框改为全选或全不选函数
+function changeBoxCheck() {
+	if (checkAllBox.checked) {
+		for (var index = 0; index < checkboxes.length; index++) {
+			checkboxes[index].checked = true;
 		}
-		if(sum==allOrder)	checkAllBox.checked=true;
-		else	checkAllBox.checked=false;
-	}
-	//点击确认接单时执行的函数
-	function checkSelectBox(){
-		var selectBox=($("#selectChoose").val());
-		alert(selectBox);
-		for(var index=0;index<checkboxes.length;index++){
-			if(checkboxes[index].checked){
-				var id=$(checkboxes[index]).parent().next().text();	
-				alert(id);
-			}
-		}
-	}
-	//点击确认送达时执行的函数
-	function checkBox(){
-		for(var index=0;index<checkboxes.length;index++){
-			if(checkboxes[index].checked){
-				var id=$(checkboxes[index]).parent().next().text();	
-				alert(id);
-			}
+	} else if (!checkAllBox.checked) {
+		for (var index = 0; index < checkboxes.length; index++) {
+			checkboxes[index].checked = false;
 		}
 	}
+}
+function changeboxCheck() {
+	var sum = 0;
+	for (var index = 0; index < checkboxes.length; index++) {
+		if (checkboxes[index].checked == true) {
+			sum++;
+		}
+	}
+	if (sum == allOrder)
+		checkAllBox.checked = true;
+	else
+		checkAllBox.checked = false;
+}
+// 点击确认接单时执行的函数
+function checkSelectBox() {
+	var selectBox = ($("#selectChoose").val()); // 获取配送员名称
+	var idArray = new Array();
+	for (var index = 0; index < checkboxes.length; index++) {
+		if (checkboxes[index].checked) {
+			var id = $(checkboxes[index]).parent().next().text();
+			idArray.push(id); // 获取订单号
+		}
+	}
+	$.post("/LifeInGDUT/water/accept", {
+		deliver : selectBox,
+		orders : idArray.toString()
+	}, function(data, status) {
+		if (data == "success") {
+			location.reload();
+		}
+	});
+}
+// 点击确认送达时执行的函数
+function checkBox() {
+	var idArray = new Array();
+	for (var index = 0; index < checkboxes.length; index++) {
+		if (checkboxes[index].checked) {
+			var id = $(checkboxes[index]).parent().next().text();
+			idArray.push(id);
+		}
+	}
+	$.post("/LifeInGDUT/water/finish", {
+		orders : idArray.toString()
+	}, function(data, status) {
+		if (data == "success") {
+			location.reload();
+		}
+	});
+}
--- src/main/webapp/WEB-INF/jsp/untreatedOrder.jsp
+++ src/main/webapp/WEB-INF/jsp/untreatedOrder.jsp
@@ -1,16 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %> 
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html lang="zh-cn">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>送水后台管理界面</title>
-    <link href="/LifeInGDUT/css/water.css" rel="stylesheet">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>送水后台管理界面</title>
+<link href="/LifeInGDUT/css/water.css" rel="stylesheet">
 </head>
-<body onload='pagesNumberSet(1,16)'>
+<body onload='pagesNumberSet(${page},${count},${state})'>
 	<!--页面遮罩层-->
 	<div id="mask"></div>
 	<!--开头部分-->
@@ -26,29 +26,31 @@
 			<ul class="first-menu">
 				<li><a href="#" class="first-menu-li">全部订单</a>
 					<ul class="second-menu">
-						<li class="chooseBgColor"><a href="/LifeInGDUT/water/getOrder?state=-1&page=1">未接订单</a></li>
+						<li class="chooseBgColor"><a
+							href="/LifeInGDUT/water/getOrder?state=-1&page=1">未接订单</a></li>
 						<li><a href="/LifeInGDUT/water/getOrder?state=0&page=1">已接订单</a></li>
 						<li><a href="/LifeInGDUT/water/getOrder?state=1&page=1">送达订单</a></li>
-					</ul>
-				</li>
+					</ul></li>
 				<li><a href="#" class="first-menu-li">充值</a>
 					<ul class="second-menu">
 						<li><a href="/LifeInGDUT/water/charge">水费充值</a></li>
 						<li><a href="/LifeInGDUT/water/getChargeRecord?page=1">充值订单</a></li>
-					</ul>
-				</li>
+					</ul></li>
 			</ul>
 		</div>
 		<!--数据显示部分-->
 		<div class="water-data">
 			<div class="water-data-header">
 				<p>您当前的位置:未送订单</p>
-				<p class="returnLogin"><a href="login.html">退出登录</a></p>
+				<p class="returnLogin">
+					<a href="/LifeInGDUT/water/logout">退出登录</a>
+				</p>
 			</div>
 			<table border="1px" width="100%" cellspacing="0">
 				<thead>
 					<tr>
-						<td><input type="checkbox" name="checkallbox" value="checkbox" id="checkallbox"/></td>
+						<td><input type="checkbox" name="checkallbox"
+							value="checkbox" id="checkallbox" /></td>
 						<td>水单号</td>
 						<td>宿舍</td>
 						<td>学号</td>
@@ -59,20 +61,20 @@
 					</tr>
 				</thead>
 				<tbody>
-					<form action="" id="accept">
+					<form>
 						<c:forEach items="${orders}" var="order">
 							<tr>
-								<td>
-									<input type="checkbox" name="checkbox" value="checkbox" class="checkbox"/></td>
+								<td><input type="checkbox" name="checkbox" value="checkbox"
+									class="checkbox" /></td>
 								<td>${order.id}</td>
 								<td>${order.dormitory}</td>
 								<td>${order.studentId }</td>
 								<td>${order.number}</td>
 								<td>${order.sphone}</td>
 								<td>${order.time}</td>
-								<td>
-									<a><span class="delete-button">删除记录</span></a>
-								</td>
+								<td><a
+									href="/LifeInGDUT/water/delete?orderId=${order.id}&page=${page}&state=${state}"><span
+										class="delete-button">删除记录</span></a></td>
 							</tr>
 						</c:forEach>
 					</form>
@@ -81,28 +83,26 @@
 			<!--分页部分及修改状态部分-->
 			<div class="water-data-pages">
 				<div class="selectBox">
-					选择配送员:
-					<select id="selectChoose" name="deliver" form="accept">
-						<option value="A">配送员A</option>
-						<option value="B">配送员B</option>
-						<option value="C">配送员C</option>
-						<option value="D">配送员D</option>
-					</select>
-					<span class="acceptOrder">确认接单</span>
+					选择配送员: <select id="selectChoose" name="deliver" form="accept">
+						<option value="张三">张三</option>
+						<option value="李四">李四</option>
+						<option value="王五">王五</option>
+						<option value="赵六">赵六</option>
+					</select> <span class="acceptOrder">确认接单</span>
 				</div>
-				<div id="setpage"></div> 
+				<div id="setpage"></div>
 			</div>
 			<!--订单详情-->
 			<div id="more-infor">
 				<div class="more-infor-head">
 					<p>订单详情</p>
 					<div class="more-infor-button">关闭</div>
-				</div>	
+				</div>
 				<div id="moreInfor-Table"></div>
 			</div>
 		</div>
 	</div>
 </body>
-<script src="js/jquery.js"></script>
-<script src="js/water.js"></script>
+<script src="/LifeInGDUT/js/jquery.js"></script>
+<script src="/LifeInGDUT/js/water.js"></script>
 </html>
